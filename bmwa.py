5b7fb421df1c01d07ffd44b7ba65ad868bceaf56
Matthias Jasny <matthiasjasny@gmail.com>
2013-10-04 19:24:10 +0200

    Added Template test

--- bmwa.py
+++ bmwa.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Matthias' } # fake user
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
 
 if __name__ == '__main__':
-#    app.debug = True
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0',port=5000)
     
