a1cd3e0c7db6b09c3ada440a6e6341a0dde4c1d6
Matthias Jasny <matthiasjasny@gmail.com>
2013-10-05 11:13:01 +0200

    First experiments with decoding Messages successfull

--- bmwa.py
+++ bmwa.py
@@ -1,14 +1,25 @@
 from flask import Flask
+from flask import render_template
+import xmlrpc.client
+import json
+import base64
+
 app = Flask(__name__)
 app.config.from_object('config')
 
+
 @app.route('/')
-@app.route('/index')
 def index():
-    user = { 'nickname': 'Matthias' } # fake user
-    return render_template("index.html",
-        title = 'BitMessageWebApp',
-        user = user)
+    api = xmlrpc.client.ServerProxy("http://matthias:password@192.168.178.5:8442/")
+    inboxMessages = json.loads(api.getAllInboxMessages())
+
+    data = 'Messages in inbox: ' + str(len(inboxMessages['inboxMessages']))
+    for i in range(0,len(inboxMessages['inboxMessages'])):
+        data+= ' ['+str(i)+'] '+base64.b64decode(inboxMessages['inboxMessages'][i]['message'].encode('ascii')).decode('utf-8')
+
+    #data = base64.b64decode(inboxMessages['inboxMessages'][0]['message'].encode('ascii'))
+
+    return render_template("index.html", title = 'BMWA-DEV', data = data)
 
 if __name__ == '__main__':
     app.debug = True
