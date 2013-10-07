e06a012b8e1319cc78534d3357855925c1711b22
tullyvey <ralph.smithen@gmail.com>
2013-10-07 17:31:49 +0100

    Changed inbox vew and added prototype message view.

--- bmwa.py
+++ bmwa.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request
 import xmlrpc.client
 import json
 import base64
@@ -9,22 +8,43 @@ import base64
 app = Flask(__name__)
 app.config.from_object('config')
 
+CONN_STRING = "http://%s:%s@%s:%d" % (app.config['API_USER'],
+    app.config['API_PASSWD'], app.config['API_HOST'], app.config['API_PORT'])
+
+
+def _b64decode(s):
+    """Helper function to decode base64 unicode."""
+    return base64.b64decode(s.encode('ascii')).decode('utf-8')
+
 
 @app.route('/')
 def index():
-    data = 'Click on Inbox Show'
-    return render_template("index.html", data = data)
+    api = xmlrpc.client.ServerProxy(CONN_STRING)
+    messages = json.loads(api.getAllInboxMessages())['inboxMessages']
+    for m in messages:
+        m['subject'] = _b64decode(m['subject'])
+    return render_template("index.html", messages=messages)
+
+
+@app.route('/view/<msgid>')
+def view(msgid):
+    api = xmlrpc.client.ServerProxy(CONN_STRING)
+    message = json.loads(api.getInboxMessageByID(msgid))
+    return render_template("view.html", data=message)
+
+
 
 @app.route('/inbox_show')
 def index_show():
-    api = xmlrpc.client.ServerProxy("http://matthias:password@192.168.178.5:8442/")
+    api = xmlrpc.client.ServerProxy(CONN_STRING)
     inboxMessages = json.loads(api.getAllInboxMessages())
     messageid = None
     if 'messageid' in request.args:
-        messageid = request.args.get('messageid')   
+        messageid = request.args.get('messageid')
 
-    if messageid is not None:   
-        return base64.b64decode(inboxMessages['inboxMessages'][int(messageid)]['message'].encode('ascii')).decode('utf-8')
+    if messageid is not None:
+        return _b64decode(
+            inboxMessages['inboxMessages'][int(messageid)]['message'])
 
     data = 'Messages in inbox: ' + str(len(inboxMessages['inboxMessages']))
     for i in range(0,len(inboxMessages['inboxMessages'])):
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 #Config File
+
+API_USER = 'matthias'
+API_PASSWD = 'password'
+API_HOST = 'localhost'
+API_PORT = 8442
+
