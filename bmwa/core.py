1be5895305589cea7aff97bff43a370624bdbcf2
tullyvey <ralph.smithen@gmail.com>
2013-10-10 12:01:43 +0100

    Add environment variable config loading.

--- bmwa/core.py
+++ bmwa/core.py
@@ -4,4 +4,4 @@ from . import config
 
 app = Flask(__name__.split('.')[0])
 app.config.from_object(config.__name__)
-#app.config.from_envvar('BMWA_SETTINGS', silent=True)
+app.config.from_envvar('BMWA_SETTINGS', silent=True)
