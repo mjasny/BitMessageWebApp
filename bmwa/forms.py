4f3a497f276ed63119d6805d6d9c0c8b7c99c9d0
tullyvey <ralph.smithen@gmail.com>
2013-10-15 12:30:56 +0100

    Add addressbook entry to forms

--- bmwa/forms.py
+++ bmwa/forms.py
@@ -1,6 +1,26 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField, SelectField
-from wtforms.validators import Required
+from wtforms.validators import Required, ValidationError
+
+
+BASE58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+
+def validate_bitmessage_address(form, field):
+    address = field.data
+
+    if address.lower().startswith('bm-'):
+        address = address[3:]
+
+    length = len(address)
+    if length < 28:
+        raise ValidationError("Length of address seems too short.")
+    elif length > 36:
+        raise ValidationError("Length of address seems too long.")
+
+    for c in address:
+        if c not in BASE58_ALPHABET:
+            raise ValidationError("Invalid character in address.")
 
 
 class SendForm(Form):
@@ -8,3 +28,9 @@ class SendForm(Form):
     from_address = SelectField('From')
     subject = StringField('Subject', validators=[Required()])
     message = TextAreaField('Message', validators=[Required()])
+
+
+class AddressBookEntry(Form):
+    address = StringField('Address',
+                validators=[Required(), validate_bitmessage_address])
+    label = StringField('Label', validators=[Required()])
